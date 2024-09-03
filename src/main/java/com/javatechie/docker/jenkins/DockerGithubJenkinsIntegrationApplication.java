b7b1b077d7ab58ba07db94f8e6cd9436e522db59
sgreddy.vardhan16 <sgreddy.vardhan16gail.com>
2024-09-03 15:46:10 +0530

    First Commit

---
+++ src/main/java/com/javatechie/docker/jenkins/DockerGithubJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGithubJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGithubJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/docker/jenkins/DockerGithubJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGithubJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
